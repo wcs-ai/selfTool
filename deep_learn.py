f2d1e7e8e091dc85f611fbdc7d722835408af5e3
xiaoswuwei <1815161966@qq.com>
2019-09-01 23:21:33 +0800

    deep_learn

---
+++ deep_learn.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# # -*- coding: UTF-8 -*-
+import tensorflow as tf
+from tensorflow.contrib.layers.python.layers import batch_norm
+
+class CNN(object):
+    #transmit a list or array that be createrd layers' arguments
+    def __init__(self,wts_size,bas_size):
+        assert type(wts_size)==tuple or type(wts_size)==list,"wts_size can't iterable"
+        assert type(bas_size)==tuple or type(bas_size)==list,"bas_size can't iterable"
+
+        self.wts = []
+        self.bas = []
+        self.convole_layers = []
+        self.pool_layers = []
+        for sw,sb in zip(wts_size,bas_size):
+            self.wts.append(self.create_wt(sw))
+            self.bas.append(self.create_ba(sb))
+            self.convole_layers.append(None)
+            self.pool_layers.append(None)
+
+    def create_wt(self,size):
+        res = tf.random_normal(size,stddev=1,mean=0)
+        return tf.Variable(res,dtype=tf.float32)
+    def create_ba(self,size):
+        bias = tf.random_uniform(size,minval=0,maxval=1)
+        return tf.Variable(bias,dtype=tf.float32)
+    #封装了归一化、激活的卷积操作
+    def conv2d(self,IMG,FILTER,bas,training,STRIDE=[1,1,1,1],PADDING='SAME'):
+        cvd = tf.nn.conv2d(IMG,FILTER,strides=STRIDE,padding=PADDING) + bas
+        norm_cvd = batch_norm(cvd,decay=0.9,is_training=training)
+        elu_cvd = tf.nn.relu(norm_cvd)
+        return elu_cvd
+    def avage_pool(self,img,ksize=[1,3,3,1],stride=[1,2,2,1],PADDING='SAME'):
+        pool = tf.nn.avg_pool(img,ksize=ksize,strides=stride,padding=PADDING)
+        return pool
+    def struct_model(self,img,training):
+        with tf.device('/gpu:0'):
+            for i in range(len(self.bas)):
+                self.convole_layers[i] = self.conv2d(img,self.wts[i],self.bas[i],training=training)
+                self.pool_layers[i] = 1
+
+
+class RNN(object):
+    def __init__(self):
+        print('rnn')
+
--- face.py
+++ face.py
@@ -3,6 +3,7 @@ from SELF_TOOLS import common
 import cv2
 import scipy
 
+
 class discern_face(object):
     def __init__(self,imgs,haar_path):
         self.img_path = imgs
@@ -17,8 +18,9 @@ class discern_face(object):
             self.imgs.append(img_data)
             img_gray = cv2.cvtColor(img_data,cv2.COLOR_BGR2GRAY)
             face_descade = cv2.CascadeClassifier(self.haar_file)
-            faces = face_descade.detectMultiScale(img_gray,1.3,5)
+            faces = face_descade.detectMultiScale(img_gray,1.2,3)
             coordinates.append(faces)
+
         return coordinates
     #get face data,arguments:output img size
     def get_face_data(self,size,batch=0):
@@ -27,11 +29,12 @@ class discern_face(object):
         i = 1
         #获取人脸部位数据，并统一大小
         for item,img in zip(cds,self.imgs):
-            image = img[item[1]:item[1]+item[3],item[0]:item[0]+item[2]]
+            #image = img[item[1]:(item[1]+item[3]),item[0]:(item[0]+item[2]),:]
+            image = img[69:180,69:180,:]
             image_resize = cv2.resize(np.array(image),size,fx=0.25,fy=0.25,interpolation=cv2.INTER_AREA)
             batch_arr.append(image_resize)
             #如果batch为0就不当成迭代对象使用
-            if batch==0:
+            if batch>0:
                 if i % batch == 0 or len(self.imgs) <= i:
                     yield batch_arr
             else:
