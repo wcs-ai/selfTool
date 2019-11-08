820de6bc07b8d3d14eed7653fc9cef7bb8c423db
xiaoswuwei <1815161966@qq.com>
2019-11-08 18:12:16 +0800

    add seq

--- deep_learn.py
+++ deep_learn.py
@@ -7,65 +7,83 @@ import numpy as np
 __UNIFY_FLOAT__ = tf.float32
 
 class __Basic_net__(object):
-    def __init__(self):
-        self.BATCH_SIZE = 300
-        self.START_LEARN_RATE = 0.1
-        self.TRAIN_CORD = {}
-        self.SAVE_PATH = 'data/model.cpkt'
-        self.CONVOL = 1
+    def __argument():
+        info = {
+            "batch":200,
+            "save_path":'data/',
+            "start_learn":1e-3
+        }
+        return info    
 
+    #低版本tensorflow没有该激活函数，自己封装的。
+    def Swish(x,beta=1):
+        return x*tf.nn.sigmoid(x*beta)
 
-#创建，随机生成参数
-def create_weight(size,dtype=__UNIFY_FLOAT__,name="weight"):
-    res = tf.truncated_normal(size,stddev=1,mean=0,dtype=dtype)
-    return tf.Variable(res,dtype=dtype)
-def create_bias(size,dtype=__UNIFY_FLOAT__,name="bias"):
-    bias = tf.constant(0.1,shape=size,dtype=dtype,name=name)
-    return tf.Variable(bias,dtype=dtype,name=name)
+    #创建，随机生成参数
+    def create_weight(size,dtype=__UNIFY_FLOAT__,name="weight"):
+        res = tf.truncated_normal(size,stddev=0.2,mean=1,dtype=dtype)
+        return tf.Variable(res,dtype=dtype)
 
-#封装了归一化、激活的卷积操作,数据、卷积核、偏置值、激活函数、是否是训练状态、滑动步长
-def conv2d(data,nucel,bias=0,activate_function=tf.nn.relu,training=True,strides=[1,1,1,1],PADDING='SAME'):
-    x = tf.nn.dropout(data,0.8)
-    cvd = tf.nn.conv2d(x,nucel,strides=strides,padding=PADDING)
-    if bias!=0:
-        cvd = tf.nn.bias_add(cvd,bias)
+    def create_bias(size,dtype=__UNIFY_FLOAT__,name="bias"):
+        bias = tf.constant(0.1,shape=size,dtype=dtype,name=name)
+        return tf.Variable(bias,dtype=dtype,name=name)
 
-    norm_cvd = batch_norm(cvd,decay=0.8,is_training=training)
-    #norm_cvd = cvd
-    elu_cvd = activate_function(norm_cvd)
-    return elu_cvd
+    #检查模型是否保存，及其迭代次数
+    def check_point(save_path):
+        #若保存，路径下会有一个checkpoint文件
+        kpt = tf.train.latest_checkpoint(save_path)
+        if kpt!=None:
+            ind = kpt.find('-')
+            step = int(kpt[ind+1:])
+            return (True,step)
+        else:
+            return (False,0)
+    #优化器
+    def take_optimize(loss,learn_rate=0.1,method="ADM"):
+        if method=="ADM":
+            _optimize = tf.train.AdamOptimizer
+        return _optimize(learn_rate).minimize(loss)
 
+    #计算损失值
+    def calc_loss(labels,logits,back="mean",method="softmax"):
+        loss = ''
+        if method=="softmax":
+            loss = tf.nn.softmax_cross_entropy_with_logits(labels=labels,logits=logits)
+        else:
+            loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels,logits=logits)
 
-#低版本tensorflow没有该激活函数，自己封装的。
-def Swish(x,beta=1):
-    return x*tf.nn.sigmoid(x*beta)
+        if back=="mean":
+            return tf.reduce_mean(loss)
+        else:
+            return tf.reduce_sum(loss)
 
-#计算损失值
-def calc_loss(labels,logits,back="mean",method="softmax"):
-    loss = ''
-    if method=="softmax":
-        loss = tf.nn.softmax_cross_entropy_with_logits(labels=labels,logits=logits)
-    else:
-        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels,logits=logits)
+    #封装了归一化、激活的卷积操作,数据、卷积核、偏置值、激活函数、是否是训练状态、滑动步长
+    def conv2d(data,nucel,bias=0,activate_function=tf.nn.relu,training=True,strides=[1,1,1,1],PADDING='SAME'):
+        x = tf.nn.dropout(data,0.9)
+        cvd = tf.nn.conv2d(x,nucel,strides=strides,padding=PADDING)
+        if bias!=0:
+            cvd = tf.nn.bias_add(cvd,bias)
 
-    if back=="mean":
-        return tf.reduce_mean(loss)
-    else:
-        return tf.reduce_sum(loss)
+        norm_cvd = batch_norm(cvd,decay=0.9,is_training=training)
+        #norm_cvd = cvd
+        elu_cvd = activate_function(norm_cvd)
+        return elu_cvd
 
-#计算精确度
-def calc_accuracy(logits,labels):
-    logit_max = tf.argmax(logits,1)
-    label_max = tf.argmax(labels,1)
-    eq = tf.cast(tf.equal(logit_max,label_max),tf.float32)
-    return tf.reduce_mean(eq)
+    #计算精确度
+    def calc_accuracy(logits,labels):
+        logit_max = tf.argmax(logits,1)
+        label_max = tf.argmax(labels,1)
+        eq = tf.cast(tf.equal(logit_max,label_max),tf.float32)
+        return tf.reduce_mean(eq)
 
-#优化器
-def take_optimize(loss,learn_rate=0.1,method="ADM"):
-    if method=="ADM":
-        _optimize = tf.train.AdamOptimizer
+    def max_pool(data,ksize=[1,3,3,1],strides=[1,2,2,1]):
+        pool = tf.nn.max_pool(data,ksize=ksize,strides=strides,padding="SAME")
+        return pool
 
-    return _optimize(learn_rate).minimize(loss)
+    def avg_pool(data,ksize=[1,3,3,1],stride=[1,2,2,1],PADDING='SAME'):
+        pool = tf.nn.avg_pool(data,ksize=ksize,strides=stride,padding=PADDING)
+        return pool
+
 
 #用于测试时建立的session
 def test_session(data,init=False):
@@ -75,31 +93,14 @@ def test_session(data,init=False):
     res = sess.run(data)
     return res
 
-def max_pool(data,ksize=[1,3,3,1],strides=[1,2,2,1]):
-    pool = tf.nn.max_pool(data,ksize=ksize,strides=strides,padding="SAME")
-    return pool
 
-def avg_pool(data,ksize=[1,3,3,1],stride=[1,2,2,1],PADDING='SAME'):
-    pool = tf.nn.avg_pool(data,ksize=ksize,strides=stride,padding=PADDING)
-    return pool
-
-#检查模型是否保存，及其迭代次数
-def check_point(save_path):
-    #若保存，路径下会有一个checkpoint文件
-    kpt = tf.train.latest_checkpoint(save_path)
-    if kpt!=None:
-        ind = kpt.find('-')
-        step = int(kpt[ind+1:])
-        return (True,step)
-    else:
-        return (False,0)
-
-
-class Cnn(object):
+#卷积神经网络
+class Cnn(__Basic_net__):
     #transmit a list or array that be createrd layers' arguments
     def __init__(self):
         self.op = 'rnn'
         self.ACTIVATE_FUNCTION = Swish
+        self.ARG = __Basic_net__.__argument()
 
     def multi_layer(self,data,weights,biass):
         argument_num = len(biass)
@@ -121,6 +122,7 @@ class Cnn(object):
         res = tf.nn.max_pool(img,ksize=ksize,strides=stride,padding=PADDING)
         return res,mask
 
+
     #反最大池化与反平均池化
     def unpool(self,tp,fw,step=None,padding="SAME"):
         #默认滑动步长为2
@@ -140,8 +142,65 @@ class Cnn(object):
         return tf.reshape(avg,shape)
         #return last_res
 
-
+#普通的循环神经网络构建
 class Rnn(object):
     def __init__(self):
         print('rnn')
 
+
+#使用新版接口构建的seq2seq模型
+class Seq2seq(__Basic_net__):
+    def __init__(self,encoder_input,decoder_input,hidden_size=3,unite=60,inference=False):
+        self.enp = encoder_input
+        self.dep = decoder_input
+        self.INFERENCE = inference
+        self.HIDDEN_NUM = hidden_size
+        self.CELL_UNITE = unite
+        self.encode_result = ''
+        self.encode_state = ''
+        self.ARG = __Basic_net__.__argument()
+
+        self.encode_cell = self.cell()
+        self.decode_cell = self.cell()
+        self.encoder()
+
+    def encoder(self):
+        #用动态rnn构建,encode_state的维度为[batchsize,num_units]
+        self.encode_result,self.encode_state = tf.nn.dynamic_rnn(self.encode_cell,self.enp,dtype=UNIFY_FLOAT)
+
+    def decoder(self,seq_length,model="decoder",start_token=None,end_token=None):
+        #为decode层构建一个全连接层，得出每个序列后在乘以该全连接层，把最后的维度转为vocab_len而不是unite数
+        #project_layer = tf.layers.Dense(units=200,kernel_initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.1))
+        project_layer = Dense(200)
+        if not self.INFERENCE:
+            #seq_len = tf.placeholder(tf.int32, shape=[None], name='batch_seq_length')
+            #sequence_length:当前batch中每条数据的序列数量，超出的话会报错：Tried to read from index 20 but array size is: 20
+            helper = TrainingHelper(self.dep,seq_length,time_major=False)
+        else:
+            helper = GreedyEmbeddingHelper(self.dep,start_token,end_token)
+        if model=="decoder":
+            train_deocde = BasicDecoder(cell=self.decode_cell,helper=helper,output_layer=project_layer,initial_state=self.encode_state)
+        else:
+            train_deocde = BasicDecoder(cell=self.decode_cell,helper=helper,output_layer=project_layer,
+                initial_state=self.decode_cell.zero_state(10,tf.float32))
+        #final_sequence_lengths是一个一维数组，每一条数据的序列数量。
+        logits,final_state,final_sequence_lengths = dynamic_decode(train_deocde,output_time_major=True,impute_finished=True)
+        return logits,final_state,final_sequence_lengths
+
+    def attention_decoder(self,encode_seq_num,decode_seq_num):
+        #num_units与cell中的num_units一致，用于一个全连接权重的列数,与encode层的cell的num_units一样大小。
+        attention_mechanism = LuongAttention(num_units=self.CELL_UNITE,memory=self.encode_result,memory_sequence_length=encode_seq_num)
+        #alignment_history表示每一步的alignment是否存储到state中，tenrsorbord可视化时可用,
+        #cell_input_fn为一个函数，默认将input和上一步的attention拼接起来送入cell
+        self.decode_cell = AttentionWrapper(cell=self.decode_cell,attention_mechanism=attention_mechanism,
+            attention_layer_size=None,alignment_history=False)
+
+        logits,final_state,final_sequence_lengths = self.decoder(decode_seq_num,model="attention_decoder")
+        return logits
+ 
+    def cell(self):
+        cells = []
+        for i in range(self.HIDDEN_NUM):
+            cells.append(tf.contrib.rnn.GRUCell(self.CELL_UNITE))
+        mcell = tf.contrib.rnn.MultiRNNCell(cells)
+        return mcell
